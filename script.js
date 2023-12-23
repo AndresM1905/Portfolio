7d2566fb0ff7560d24964e4b8e60e869c7cecc17
AndresMartinez <carlosandres190599@gmail.com>
2023-12-23 04:12:47 -0600

    cierreDeNavCunadoSeClickeaAfuera

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ boton.addEventListener("click",()=>{
     nav.classList.toggle("open");
 });
 
+document.querySelector('nav').addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
 
 document.addEventListener("scroll",()=>{
 topCentro=document.querySelector("#portfolio").getBoundingClientRect().top;
@@ -13,6 +17,13 @@ buttomNav=nav.getBoundingClientRect().bottom;
     if(buttomNav>topCentro && nav.classList.contains("open")){
         nav.classList.toggle("open");
     }
+
+});
+
+window.addEventListener("click",()=>{
+if(nav.classList.contains("open")){
+    nav.classList.remove("open") ; 
+};
 
 });
 
