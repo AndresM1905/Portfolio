d47d8fb62167c4d18627e72d2de0ea35cf1edb06
AndresMartinez <carlosandres190599@gmail.com>
2024-01-01 13:47:43 -0600

    actualziado css con webpack

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ import "./assets/Scripts/CargaElementos.mjs";
 import "./assets/Scripts/listenerExperience.mjs";
 import "./node_modules/webcimes-modal/dist/js/webcimes-modal.esm.js";
 import "./assets/Scripts/script.mjs";
+/* */
