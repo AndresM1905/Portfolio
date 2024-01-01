1cb1dfeeb7672e57732f049e503375218ef94747
AndresMartinez <carlosandres190599@gmail.com>
2024-01-01 13:22:06 -0600

    carga antes css que js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "./assets/css/index.css";
 import "./assets/Scripts/scriptNav.mjs";
 import "./assets/Scripts/scriptImgCircle.mjs";
 import "./assets/Scripts/scriptH1.mjs";
@@ -11,5 +12,3 @@ import "./assets/Scripts/CargaElementos.mjs";
 import "./assets/Scripts/listenerExperience.mjs";
 import "./node_modules/webcimes-modal/dist/js/webcimes-modal.esm.js";
 import "./assets/Scripts/script.mjs";
-import "./assets/css/index.css";
-//require ("./assets/video/hero.mp4");
